dfe916cc1bb0f876ffe6d14b8e5dda97dda33903
AnaPinto <Ana.Pinto@Fundacion-jala.org>
2016-01-25 19:19:55 -0400

    JS Task 3

---
+++ Task3.js
@@ -0,0 +1,13 @@
+/**
+ * Created by AnaPinto on 1/25/2016.
+ */
+
+var countWords = function (myparagraph){
+
+    return myparagraph.length;
+}
+
+var paragraph = "Hello World!";
+var totalWords = countWords(paragraph);
+console.log("The number of words is: " + totalWords);
+
