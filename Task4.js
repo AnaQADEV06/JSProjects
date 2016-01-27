5536e14a5c2807f992cae2e2a675d05748e51472
AnaPinto <Ana.Pinto@Fundacion-jala.org>
2016-01-27 14:36:34 -0400

    JS Task 4 in progress
    NOTE: Yesterday I forget to upload this commit
    This commit corresponds to  01/26/2016 date
    
    Regards,
    
    Ana

---
+++ Task4.js
@@ -0,0 +1,31 @@
+/**
+ * Created by AnaPinto on 1/27/2016.
+ */
+
+var date1 = new Date();
+
+var day = date1.getDay();
+
+switch (day){
+    case 1:
+        console.log("Today is: Monday !");
+        break;
+    case 2:
+        console.log("Today is: Tuesday !");
+        break;
+    case 3:
+        console.log("Today is: Wednesday !");
+        break;
+    case 4:
+        console.log("Today is: Thursday !");
+        break;
+    case 5:
+        console.log("Today is: Friday !");
+        break;
+    case 6:
+        console.log("Today is: Saturday !");
+        break;
+    case 7:
+        console.log("Today is: Sunday !");
+        break;
+}
