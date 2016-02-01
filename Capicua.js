1396e397f79a0285ad53e1f17acc1b8db8fa3522
AnaPinto <Ana.Pinto@Fundacion-jala.org>
2016-02-01 19:50:18 -0400

    lookCapicua function completed and tested

--- Capicua.js
+++ Capicua.js
@@ -4,7 +4,7 @@
 
 /*Test*/
 
-
+/*
 var test = 121;
 var compareTest1 = ((test.toString()).split("")).toString();
 var compareTest2 = (((test.toString()).split("")).reverse()).toString();
@@ -18,3 +18,29 @@ if(compareTest1 === compareTest2){
 else{
     console.log(test + " no es un numero capicua")
 }
+*/
+
+
+/*Commint Monday 01 Feb*/
+
+/*lookCapicua function completed and tested */
+
+var lookCapicua = function(i, f){
+
+    var f = f;
+    for (var i = i; i <= f; i++) {
+
+
+        var compare1 = ((i.toString()).split("")).toString();
+        var compare2 = (((i.toString()).split("")).reverse()).toString();
+
+        if(compare1 === compare2){
+
+            console.log( i + " is a capicua number!");
+            break;
+        }
+
+    }
+
+};
+
